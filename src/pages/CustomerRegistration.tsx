7de9aa52b84e594a186ce5a120e18a5cb2aad3f6
aryapatil-edba <arya.patil@edba.io>
2026-07-28 14:03:13 +0530

    arya resolves build error

--- src/pages/CustomerRegistration.tsx
+++ src/pages/CustomerRegistration.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { CheckCircle2, User, Home, FileText, Send, Building } from 'lucide-react';
+import { CheckCircle2, User, Home, FileText, Building } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
